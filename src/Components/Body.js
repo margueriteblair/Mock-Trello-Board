49a2f668ae022f5751a564209daa8fcd13375ba9
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-29 23:39:00 -0400

    mapped over list array to provide objects

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import ListItem from './ListItem';
+import { ListContext } from '../Contexts/ListContext'
 
 export default function Body() {
     return (
         <div>
-            <h1>Body</h1>
+            <h1>List of Todos:</h1>
+            {
+                list.map((value, index) => {
+                    return (
+                        <ListItem checked={value.checked} data={value.checked} id={index} key={index}></ListItem>
+                    )
+                })
+            }
+
+            <div className="form">
+                <label>Add task:</label>
+                
+            </div>
         </div>
     )
 }
