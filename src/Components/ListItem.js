dfd5d63434d8358c0bfec8227742261f5930532f
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-29 23:19:28 -0400

    ListItem component with a checkItem method

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ListContext } from '../Contexts/ListContext'
 
-export default function ListItem() {
+export default function ListItem(props) {
+
+    const {list, setList} = useContext(ListContext);
+
+    const checkItem = () => {
+        const newList = [...list];
+        list[props.id].checked = !list[props.id].checked;
+        setList(newList)
+    }
+
     return (
         <div>
             
