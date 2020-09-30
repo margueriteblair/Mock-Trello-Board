0cc26a2d1d3e2fb7f96a8f14d97b1e4c0ee8daa9
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-29 22:56:29 -0400

    created a list context with usestate and useeffect

---
+++ src/Contexts/ListContext.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+
+export const ListContext = React.createContext();
+
+export default function ListContextProvider(props) {
+    const [state, setState] = useState(() => {
+        return JSON.parse(localStorage.getItem('Todo-List')) ?? [{data: "Sample Text", checked: false}]
+    //use of the null coalescing operator
+    //if the left hand arg is null or undefined, it returns the right hand
+    //if defined, returns the left hand
+    
+    });
+
+    useEffect(() => {
+        localStorage.setItem("Todo-List", JSON.stringify(state))
+    }, [state])
+
+    return (
+        <ListContent.Provider value={{list: state, setList: setState}}>
+            { props.children }
+        </ListContent.Provider>
+    )
+}
